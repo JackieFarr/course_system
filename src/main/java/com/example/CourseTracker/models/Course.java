16d31b745211b06d1efc74f1f17a6b9dfb5fc5d6
Jennifer Proctor <jen_p_13@hotmail.co.uk>
2018-12-06 11:10:35 +0000

    Added Course model

---
+++ src/main/java/com/example/CourseTracker/models/Course.java
@@ -0,0 +1,84 @@
+package com.example.CourseTracker.models;
+
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "courses")
+public class Course {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "town")
+    private String town;
+
+    @Column(name = "rating")
+    private int rating;
+    
+    @JsonIgnore
+    @OneToMany(mappedBy = "course", fetch = FetchType.LAZY)
+    private List<Booking> bookings;
+
+    public Course(String name, String town, int rating) {
+        this.name = name;
+        this.town = town;
+        this.rating = rating;
+        this.bookings = new ArrayList<>();
+    }
+
+    public Course() {
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getTown() {
+        return this.town;
+    }
+
+    public int getRating() {
+        return this.rating;
+    }
+
+    public List<Booking> getBookings() {
+        return this.bookings;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setTown(String town) {
+        this.town = town;
+    }
+
+    public void setRating(int rating) {
+        this.rating = rating;
+    }
+
+    public void setBookings(List<Booking> bookings) {
+        this.bookings = bookings;
+    }
+
+    public void addBooking(Booking booking) {
+        this.bookings.add(booking);
+    }
+}
