779e235e1c788a0b64ed5ff3f1b4062200a911f1
Jennifer Proctor <jen_p_13@hotmail.co.uk>
2018-12-06 11:56:53 +0000

    controller structures and findAll routes created

---
+++ src/main/java/com/example/CourseTracker/controllers/CourseController.java
@@ -0,0 +1,23 @@
+package com.example.CourseTracker.controllers;
+
+import com.example.CourseTracker.models.Course;
+import com.example.CourseTracker.repositories.CourseRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(value = "/courses")
+public class CourseController {
+
+    @Autowired
+    CourseRepository courseRepository;
+
+    @GetMapping
+    public List<Course> getAllCourses(){
+        return courseRepository.findAll();
+    }
+}
